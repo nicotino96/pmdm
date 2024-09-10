9f834d7abf4edf128a35da06b938ff22240474e8
Rubén Montero <ruben.montero@fpcoruna.afundacion.org>
2024-09-10 12:46:29 +0200

    Added initial files

---
+++ android-introduction/run-tests.bat
@@ -0,0 +1,29 @@
+@ECHO off
+
+call gradlew connectedAndroidTest
+SetLocal EnableDelayedExpansion
+SET tests_fail=unknown
+type "app\build\outputs\androidTest-results\connected\TEST-Nexus_S_API_28(AVD) - 9-_app-.xml" > tests_output.xml
+FOR /F "tokens=4-9 delims=^= " %%i IN (tests_output.xml) DO (
+  if [%%i] == [tests] (
+    SET tests_total=%%j
+  )
+  if [%%k] == [failures] (
+    SET tests_fail=%%l
+  )
+  if [%%m] == [errors] (
+    SET tests_errors=%%n
+  )
+)
+
+git show -s --format=%%H > commit_sha.temp
+SET /p sha= < commit_sha.temp
+SET /A tests_ok = %tests_total% - %tests_fail%
+SET /A tests_ok = %tests_ok% - %tests_errors%
+SET /A tests_total_int = %tests_total% + 0
+if [%tests_fail%] == [unknown] (
+  echo {"commit_info": "%sha%", "tests_ejecutados": "unknown", "tests_exitosos": "unknown" } > result.json
+) else (
+  echo {"commit_info": "%sha%", "tests_ejecutados": %tests_total_int%, "tests_exitosos": %tests_ok% } > result.json
+)
+
