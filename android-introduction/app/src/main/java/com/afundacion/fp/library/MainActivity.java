f76988831adf652aedf9ae89e3f88ad10c42048c
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-20 12:39:40 +0200

    Tarea 7, click en boton Tostada

--- android-introduction/app/src/main/java/com/afundacion/fp/library/MainActivity.java
+++ android-introduction/app/src/main/java/com/afundacion/fp/library/MainActivity.java
@@ -3,6 +3,7 @@ package com.afundacion.fp.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "La primera tostada del día", Toast.LENGTH_SHORT).show();
+        OnClickHandler myHandler = new OnClickHandler(this);
+        Button myButton = findViewById(R.id.toastButton);
+        myButton.setOnClickListener(myHandler);
     }
+
 }
---
+++ android-introduction/app/src/main/java/com/afundacion/fp/library/OnClickHandler.java
@@ -0,0 +1,18 @@
+package com.afundacion.fp.library;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+public class OnClickHandler implements View.OnClickListener{
+    private Context context;
+
+    public OnClickHandler(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this.context, "¡Tostada al aire!", Toast.LENGTH_LONG).show();
+    }
+}
