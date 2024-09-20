400195912c4e391ef3eab5d0ab109be39ae0fac7
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-20 11:59:06 +0200

    Tarea 6, primera tostada

--- android-introduction/app/src/main/java/com/afundacion/fp/library/MainActivity.java
+++ android-introduction/app/src/main/java/com/afundacion/fp/library/MainActivity.java
@@ -3,6 +3,7 @@ package com.afundacion.fp.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "La primera tostada del día", Toast.LENGTH_SHORT).show();
     }
 }
