1d4f73795c5c4e01ca74ff3d290b9be80dcfad72
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-09-26 13:29:28 +0200

    primer fragment (no consigo que salta la imagen)

--- android-introduction/app/src/main/java/com/afundacion/fp/library/MonstersActivity.java
+++ android-introduction/app/src/main/java/com/afundacion/fp/library/MonstersActivity.java
@@ -1,16 +1,50 @@
 package com.afundacion.fp.library;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MonstersActivity extends AppCompatActivity {
+    private Context context = this;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monsters);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Fragment1()).commit();
+        BottomNavigationView bar = findViewById(R.id.bottomNavigation);
+        bar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.item1) {
+                    Toast.makeText(context,"Primer Digimon", Toast.LENGTH_LONG).show();
+                    Fragment myFragment = new Fragment1();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, myFragment).commit();
+                }
+                if (item.getItemId() == R.id.item2) {
+                    Toast.makeText(context,"Segundo Digimon", Toast.LENGTH_LONG).show();
+                    // Hacer algo si el usuario pulsa Digimon 2
+                }
+                if (item.getItemId() == R.id.item3) {
+                    Toast.makeText(context,"Tercer Digimon", Toast.LENGTH_LONG).show();
+                    // Hacer algo si el usuario pulsa Digimon 3
+                }
+
+                return true;
+            }
+        });
+
+
     }
 }
 
