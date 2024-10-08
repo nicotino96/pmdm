4dd74d511f379dfdfdcd00484c4e4297f3ba17cd
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-08 09:53:52 +0200

    Tarea 21: modificado mensaje de respuesta

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
@@ -13,6 +13,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,12 +30,16 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Toast.makeText(context, "Response OK", Toast.LENGTH_LONG).show();
+                        try {
+                            Toast.makeText(context, "Response OK: " + response.getString("status"), Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                
+
             }
         }
 
