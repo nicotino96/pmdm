f70dddd683dc7fa9719b40717c7b77da787b0191
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-08 10:29:38 +0200

    Tarea 23: añadido snackbar

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
@@ -1,6 +1,7 @@
 package com.afundacion.fp.clips;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -10,15 +11,19 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
     private Context context = this;
+    private ConstraintLayout mainLayout;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -57,8 +62,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
         );
+        getClips();
         this.queue.add(request);
-        // Aquí lanzaremos una petición HTTP
+        this.mainLayout = findViewById(R.id.main_layout); // Lo asignamos con findViewById
+
     }
+    private void getClips() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                Server.name + "/clips",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Snackbar.make(mainLayout, "List obtained", Snackbar.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+
+                    }
+                }
+        );
+        this.queue.add(jsonArrayRequest);
+
+    }
+
 }
 
