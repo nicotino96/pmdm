c37fc72a69aa8770e71b94291d5b4c4a60fdddbc
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-18 13:30:25 +0200

    tarea 43

--- android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/RegisterActivity.java
+++ android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/RegisterActivity.java
@@ -9,10 +9,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RegisterActivity extends AppCompatActivity {
     private EditText editTextUser;
     private EditText editTextPassword;
     private Button buttonRegister;
+    private RequestQueue requestQueue;
+
 
 
     @Override
@@ -22,12 +34,45 @@ public class RegisterActivity extends AppCompatActivity {
         editTextUser = findViewById(R.id.usuario);
         editTextPassword = findViewById(R.id.password);
         buttonRegister = findViewById(R.id.boton);
+        requestQueue = Volley.newRequestQueue(this);
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(RegisterActivity.this, "Nombre: "+editTextUser.getText().toString(), Toast.LENGTH_LONG).show();
+                sendPostRequest();
             }
         });
 
     }
+    private void sendPostRequest() {
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("username", editTextUser.getText().toString());
+            requestBody.put("password", editTextPassword.getText().toString());
+            JsonObjectRequest request = new JsonObjectRequest(
+                    Request.Method.POST,
+                    Server.name + "/users",
+                    requestBody,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Toast.makeText(RegisterActivity.this,"Registro correcto",Toast.LENGTH_LONG).show();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(RegisterActivity.this, "Error en el registro: " + error.getMessage(), Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+            );
+            this.requestQueue.add(request);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 }
