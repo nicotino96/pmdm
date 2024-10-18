328c67c0182073e823f34eab3452824d94f9ea5c
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-10-18 12:17:12 +0200

    tarea 42

--- android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/RegisterActivity.java
+++ android-sessions/android-frontend-sessions/app/src/main/java/com/afundacion/fp/sessions/RegisterActivity.java
@@ -1,13 +1,33 @@
 package com.afundacion.fp.sessions;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText editTextUser;
+    private EditText editTextPassword;
+    private Button buttonRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editTextUser = findViewById(R.id.usuario);
+        editTextPassword = findViewById(R.id.password);
+        buttonRegister = findViewById(R.id.boton);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterActivity.this, "Nombre: "+editTextUser.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
