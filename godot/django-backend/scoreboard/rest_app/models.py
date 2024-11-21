f66d0487e1ea08c45a5a7b1ecf268d817bfb8779
nicotino96 <nreys23@fpcoruna.afundacion.org>
2024-11-21 10:51:36 +0100

    reiniciado juego godot: player con idle, camára y poco más

--- godot/django-backend/scoreboard/rest_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Score(models.Model):
-	id = models.AutoField(primary_key=True, editable=False)
-	username = models.CharField(max_length=50)
-	score = models.IntegerField()
-
-	def __str__(self):
-		return self.username + self.score
